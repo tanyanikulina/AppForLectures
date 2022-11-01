45ec956ecafb8d7ee5fb46c9bc34fe51256f03a3
tnikulina <tnikulina@griddynamics.com>
2022-11-02 00:41:29 +0100

    fixed visibility

--- app/src/main/java/com/example/applectureone/presentation/articles/ArticlesFragment.kt
+++ app/src/main/java/com/example/applectureone/presentation/articles/ArticlesFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -48,6 +49,7 @@ class ArticlesFragment : Fragment() {
             adapter.setData(it)
         }
         viewModel.isLoading.observe(viewLifecycleOwner) {
+            binding.rv.isVisible = !it
             if (!it)
                 binding.swipe.isRefreshing = it
 //            binding.pb.isVisible = it
