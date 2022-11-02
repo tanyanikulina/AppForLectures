bf802d9893491fb751d5820c6767a4ad367d087c
tnikulina <tnikulina@griddynamics.com>
2022-11-02 04:44:42 +0100

    Added read and write files

--- app/src/main/java/com/example/applectureone/presentation/permissions/PermissionsFragment.kt
+++ app/src/main/java/com/example/applectureone/presentation/permissions/PermissionsFragment.kt
@@ -11,21 +11,22 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.applectureone.databinding.FragmentPermissionsBinding
+import java.io.File
 
 class PermissionsFragment : Fragment() {
 
     lateinit var binding: FragmentPermissionsBinding
 
     private val requestPermissionLauncher =
-        registerForActivityResult(
-            ActivityResultContracts.RequestPermission()
-        ) { isGranted: Boolean ->
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
             if (isGranted) {
                 Toast.makeText(requireContext(), "Granted", Toast.LENGTH_SHORT).show()
             } else {
                 Toast.makeText(requireContext(), "Denied", Toast.LENGTH_SHORT).show()
             }
         }
+
+    val fileName = "filename.txt"
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,8 +39,27 @@ class PermissionsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.btnPermissions.setOnClickListener {
-            request()
+        binding.btnPermissions.setOnClickListener { request() }
+
+        with(binding) {
+
+            btnSave.setOnClickListener {
+                val text = et.text.toString()
+
+                val file = File(requireActivity().getExternalFilesDir(null), fileName)
+                file.writeText(text)
+
+                Toast.makeText(requireContext(), "Saved", Toast.LENGTH_SHORT).show()
+            }
+
+            btnRead.setOnClickListener {
+                val file = File(requireActivity().getExternalFilesDir(null), fileName)
+                val text: String = file.readText()
+
+                tv.text = text
+                Toast.makeText(requireContext(), "Read", Toast.LENGTH_SHORT).show()
+            }
+
         }
 
     }
@@ -50,14 +70,7 @@ class PermissionsFragment : Fragment() {
                 Manifest.permission.CAMERA
             ) != PackageManager.PERMISSION_GRANTED
         ) {
-            requestPermissionLauncher.launch(
-                Manifest.permission.CAMERA
-            )
-//            ActivityCompat.requestPermissions(
-//                requireActivity(), arrayOf(
-//                    Manifest.permission.CAMERA
-//                ), 1
-//            )
+            requestPermissionLauncher.launch(Manifest.permission.CAMERA)
         } else {
             Toast.makeText(requireContext(), "Already granted", Toast.LENGTH_SHORT).show()
         }
